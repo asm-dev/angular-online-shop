1db830279861bd69efb847ae49be54341c71cddd
asm-dev <anasalamancamontero@gmail.com>
2025-05-21 18:39:13 +0100

    Implement product details component

--- src/app/components/product/product-details/product-details.component.ts
+++ src/app/components/product/product-details/product-details.component.ts
@@ -1,11 +1,47 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from '../../../interfaces/product.interface';
+import { ProductService } from '../../../services/product.service';
+import { PageLayoutComponent } from '../../layout/page/page-layout.component';
 
 @Component({
   selector: 'app-product-details',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    PageLayoutComponent,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatButtonModule,
+    FormsModule,
+    MatIconModule,
+  ],
   templateUrl: './product-details.component.html',
-  styleUrl: './product-details.component.scss'
+  styleUrls: ['./product-details.component.scss'],
 })
-export class ProductDetailsComponent {
+export class ProductDetailsComponent implements OnInit {
+  product!: Product;
+  quantity = 1;
 
+  private route = inject(ActivatedRoute);
+  private productService = inject(ProductService);
+
+  ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.productService.getById(id).subscribe({
+      next: (data) => (this.product = data),
+      error: () => console.error('Error al cargar el producto'),
+    });
+  }
+
+  addToCart(): void {
+    console.log('Añadir al carrito:', this.product, 'Cantidad:', this.quantity);
+    // TODO: dispatch a NgRx
+  }
 }
