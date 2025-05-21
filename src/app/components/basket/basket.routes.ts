dddd179c85cec99cd77b1009c65b513dc4264950
asm-dev <anasalamancamontero@gmail.com>
2025-05-21 18:04:51 +0100

    Include auth guard to basket routes

--- src/app/components/basket/basket.routes.ts
+++ src/app/components/basket/basket.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { authGuard } from '../../guards/auth.guard';
 import { BasketPageComponent } from './basket-page/basket-page.component';
 import { CheckoutComponent } from './checkout/checkout.component';
 import { ConfirmationComponent } from './confirmation/confirmation.component';
@@ -7,13 +8,16 @@ export const routes: Routes = [
   {
     path: '',
     component: BasketPageComponent,
+    canActivate: [authGuard],
   },
   {
     path: 'checkout',
     component: CheckoutComponent,
+    canActivate: [authGuard],
   },
   {
     path: 'confirmation',
     component: ConfirmationComponent,
+    canActivate: [authGuard],
   },
 ];
