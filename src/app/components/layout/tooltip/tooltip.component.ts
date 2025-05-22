1521fdd83b3f5a600ddf0e0f350af21dcefeb70c
asm-dev <anasalamancamontero@gmail.com>
2025-05-22 12:33:55 +0100

    Ensure top nav shows mobile burger menu

--- src/app/components/layout/tooltip/tooltip.component.ts
+++ src/app/components/layout/tooltip/tooltip.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { RouterLink } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-tooltip',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, RouterLink],
+  imports: [
+    MatToolbarModule,
+    MatIconModule,
+    MatMenuModule,
+    MatButtonModule,
+    RouterModule,
+  ],
   templateUrl: './tooltip.component.html',
   styleUrls: ['./tooltip.component.scss'],
 })
